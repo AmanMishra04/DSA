79b88f0adb29e2208854ce447105d3348d31077e
Nikhil <nikhilbisht0026@gmail.com>
2022-10-22 19:58:45 +0530

    USe of charAt function

--- JAVA/Strings2.java
+++ JAVA/Strings2.java
@@ -3,7 +3,7 @@ which you will replace the letter ‘e’ in the original string with letter ‘
 
 import java.util.*;
 public class Strings2 {
-    public static void main(String args[]){
+    public static void main(String args[]){ 
 
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the original string: ");
