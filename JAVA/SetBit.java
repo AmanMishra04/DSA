04f220099847b4a32bf3e79f4ceb7e567716f39d
Nikhil <nikhilbisht0026@gmail.com>
2022-10-25 19:32:32 +0530

    Set a bit to 1 at a given position

---
+++ JAVA/SetBit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SetBit {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n =sc.nextInt();
+
+        System.out.print("Enter the position: ");
+        int pos= sc.nextInt();
+
+        int bitMask= 1<<pos;
+
+        int newNum= (n|bitMask);
+        
+        System.out.println("New number becomes: "+newNum);
+    }
+}
