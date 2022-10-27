c5b6c0da83868bd1a3ba5d2ebb748ac633b4cf83
Nikhil <nikhilbisht0026@gmail.com>
2022-10-27 19:30:16 +0530

    Sum of natural numbers using recurrsion

---
+++ JAVA/naturalSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class naturalSum {
+
+    public static void printSum(int i, int n, int sum){
+        if(i==n){
+            sum+=i;
+            System.out.println("Sum of the numbers is: "+sum);
+            return;
+        }
+        sum+=i;
+        printSum(i+1, n, sum);
+    }
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the limit: ");
+        int n=sc.nextInt();
+
+        printSum(1, n, 0);
+    }
+}
