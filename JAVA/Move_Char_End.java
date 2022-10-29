02efed19b9d32de3287114827945bb1dabd9745f
Nikhil <nikhilbisht0026@gmail.com>
2022-10-29 19:07:25 +0530

    Move all the X at the end of the String

--- JAVA/Move_Char_End.java
+++ JAVA/Move_Char_End.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class Move_Char_End {
-    public static void moveX(String str, int index, int count, String newString){
+    public static void moveX(String str, char ch, int index, int count, String newString){
         if(index==str.length()){
             for(int i=0; i<count; i++){
-                newString+='x';
+                newString+=ch;
             }
             System.out.println("New string is: "+newString);
             return;
         }
         char current=str.charAt(index);
-        if(current== 'x'){
+        if(current== ch){
             count++;
-            moveX(str, index+1, count, newString);
+            moveX(str, ch, index+1, count, newString);
         }
         else{
             newString+=current;
-            moveX(str, index+1, count, newString);
+            moveX(str, ch, index+1, count, newString);
         }
     }
     public static void main(String args[]){
@@ -24,6 +24,8 @@ public class Move_Char_End {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the string: ");
         String str=sc.next();
-        moveX(str, 0, 0, "");
+        System.out.print("Enter the character you want to move at the end: ");
+        char ch=sc.next().charAt(0);
+        moveX(str, ch, 0, 0, "");
     }
 }
