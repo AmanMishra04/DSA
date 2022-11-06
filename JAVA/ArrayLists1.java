fdb7be8d3f3690797b93adfc3ff7ccccb4a6cda4
Nikhil <nikhilbisht0026@gmail.com>
2022-11-06 13:30:30 +0530

    Using Add and Get function in ArrayList in java

--- JAVA/ArrayLists1.java
+++ JAVA/ArrayLists1.java
@@ -10,12 +10,14 @@ public class ArrayLists1{
         System.out.print("Enter the size of ArrayList: ");
         int n= sc.nextInt();
 
+        //Adding element at the end of the ArrayList using add function
         System.out.println("Enter the elements: ");
         for(int i=0; i<n; i++){
             int element=sc.nextInt();
             list.add(element);
         }
 
+        //Getting element and print the ArrayList using get function
         System.out.println("Elements in the ArrayList are: ");
         for(int i=0; i<list.size(); i++){
             System.out.print(list.get(i)+" ");
