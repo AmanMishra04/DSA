2ba2fb7d2b4bb119464cf73d0af9913ce49c6703
Nikhil <nikhilbisht0026@gmail.com>
2022-11-06 14:38:20 +0530

    Remove the element from an index and Sorting the ArrayList

---
+++ JAVA/ArrayList3.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class ArrayList3 {
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list=new ArrayList<Integer>();
+
+        System.out.print("Enter the size of ArrayList: ");
+        int n= sc.nextInt();
+
+        //Adding element at the end of the ArrayList using add function
+        System.out.println("Enter the elements: ");
+        for(int i=0; i<n; i++){
+            int element=sc.nextInt();
+            list.add(element);
+        }
+
+        //Getting element and print the ArrayList using get function
+        System.out.println("Elements in the ArrayList are: ");
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+
+        //Removing an element from a given index
+        System.out.print("Enter the element index to remove: ");
+        int remidx=sc.nextInt();
+        list.remove(remidx);
+
+        System.out.println("Elements in the ArrayList are: ");
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+
+        //Sorting the ArrayList
+        Collections.sort(list);
+        System.out.println("Sorted Ayyaylist: ");
+
+        System.out.println("Elements in the ArrayList are: ");
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+}        
