13732e6c893301f894a8fb8b32efcce3a4888799
Nikhil <nikhilbisht0026@gmail.com>
2022-11-19 20:07:09 +0530

    Check is a LinkedList is pallindrome

--- JAVA/LinkedList7.java
+++ JAVA/LinkedList7.java
@@ -6,45 +6,17 @@ public class LinkedList7 {
         Scanner sc= new Scanner(System.in);
         LinkedList<Integer> list= new LinkedList<>();
 
-        System.out.println("1. Insertion from First");
-        System.out.println("2. Insertion from Last");
-        System.out.println("3. Deletion from First");
-        System.out.println("4. Deletion from Last");
-        System.out.println("5. Traverse");
+        System.out.print("Enter the numner of elements: ");
+        int n= sc.nextInt();
 
-        while(true){
-            System.out.println("Enter a choice: ");
-            int choice = sc.nextInt();
-
-            switch(choice){
-                case 1:
-                System.out.println("Enter the data: ");
-                int data=sc.nextInt();
-                list.addFirst(data);
-                break;
-
-                case 2:
-                System.out.println("Enter the data: ");
-                data =sc.nextInt();
-                list.addLast(data);
-                break;
-
-                case 3:
-                list.removeFirst();
-                break;
-
-                case 4:
-                list.removeLast();
-                break;
-
-                case 5:
-                System.out.println(list);
-                break;
-
-                default:
-                System.exit(0);
-            }
+        System.out.print("Enter the elements: ");
+        while(n>0){
+            int element=sc.nextInt();
+            list.add(element);
+            n--;
         }
-        sc.close();
+
+        System.out.println(list);
+
     }
 }
