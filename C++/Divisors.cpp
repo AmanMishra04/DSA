86387362b8b1bbb93d13b5605d2dac9a9acf6b97
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-05-26 14:53:35 +0530

    Divisors of a number

---
+++ C++/Divisors.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int num;
+    vector<int> ls;
+    cin >> num;
+
+    for(int i=1; i<= sqrt(num); i++){
+
+        if(num%i == 0){
+
+            ls.push_back(i);
+
+            if(((num/i))!= i){
+                ls.push_back(num/i);
+            }
+        }    
+    }
+
+    sort(ls.begin(), ls.end());
+    for(auto it : ls){
+        cout <<it <<" ";
+    }
+}
