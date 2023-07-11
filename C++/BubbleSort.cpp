0966057385ead1bf52a91931ba11d453d31f9ef5
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-07-11 17:16:29 +0530

    BubbleSort code

--- C++/BubbleSort.cpp
+++ C++/BubbleSort.cpp
@@ -4,19 +4,17 @@ using namespace std;
 int main(){
 
     int n;
-    cout<<"Enter the size: ";
-    cin >> n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
 
     int arr[n];
-
-    cout<<"Enter the elements: ";
-    for(int i=0; i<n; i++){
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
 
-    // Bubble Sort
-    for (int i=0; i<n-1; i++){
-        for(int j=0; i<n-i-1; j++){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<=n-i-1; j++){
             if(arr[i]>arr[i+1]){
                 int temp=arr[i];
                 arr[i]=arr[i+1];
@@ -25,8 +23,8 @@ int main(){
         }
     }
 
-    cout<<"Sorted array: ";
-    for(int i=0; i<n; i++){
+    cout<<"Sorted array is: ";
+    for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
 }
