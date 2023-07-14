66773b23e3ecfc14d9cbaa006c24c91b920e82d1
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-07-14 17:25:06 +0530

    insertion sort

--- C++/InsertionSort.cpp
+++ C++/InsertionSort.cpp
@@ -13,9 +13,10 @@ int main(){
         cin>>arr[i];
     }
 
-    for(int i=1; i<n; i++){
-        int current= arr[i];
-        int j=i-1;
+    for(int i=1; i<n; i++){     // i is tracking unsorted array
+        int current= arr[i];    //current is the element to be placed at its correct position
+        int j=i-1;              //j is tracking sorted array
+        
         while(j>=0 && current< arr[j]){
             arr[j+1]=arr[j];
             j--;
