3a5c73c8c74ab4c0fa0c3b84afd1cd2afbe6d9a5
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-09-23 12:41:26 +0530

    half inverted pyramid

--- JAVA/Pattern4.java
+++ JAVA/Pattern4.java
@@ -10,7 +10,7 @@ public class Pattern4 {
         int n = sc.nextInt();
         
         for (int i = n; i >= 1; i--) {
-            for(int j = 1; j <=i; j++){
+            for(int j = 1; j <= i; j++){
                 System.out.print("*" + " ");
             }
             System.out.println();
