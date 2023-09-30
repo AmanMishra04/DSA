a1beae29d8724a0c7644eea2ce59545eae2cb6e6
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-09-30 17:15:52 +0530

    finding the smallest and largest element of the array

---
+++ JAVA/MinMax.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        
+        // Taking input from the user
+        System.out.print("Enter the elements in the array: ");
+        for (int i=0; i<n; i++){
+            array[i] = sc.nextInt();
+        }
+
+        // Finding the minimum element in the array
+        int min = array[0];
+
+        for (int i=0; i<n; i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+
+        // Finding the maximum element in the array
+        int max = array[0];
+
+        for (int i=0; i<n; i++){
+            if (array[i] > max){
+                max = array[i];
+            }
+        }
+
+        System.out.println("The minimum element in the araay is: " +min);
+        System.out.println("The maximum element in the araay is: " +max);
+
+        sc.close();
+    }
+}
